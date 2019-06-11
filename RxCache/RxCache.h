21a602e4eb513b3da67524209a002048099d604f
Alexej Nenastev <alexey.ne.bcs@mail.ru>
2019-06-11 21:45:11 +0300

    Initial Commit

---
+++ RxCache/RxCache.h
@@ -0,0 +1,19 @@
+//
+//  RxCache.h
+//  RxCache
+//
+//  Created by alexej_ne on 11/06/2019.
+//  Copyright © 2019 alexeyne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxCache.
+FOUNDATION_EXPORT double RxCacheVersionNumber;
+
+//! Project version string for RxCache.
+FOUNDATION_EXPORT const unsigned char RxCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxCache/PublicHeader.h>
+
+
